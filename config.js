4ffc9c80a42d89c7e7b993cdbcb02609b2f0b964
hoanglam4846 <87513012+hoanglam4846@users.noreply.github.com>
2021-07-16 12:57:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,10 @@
 const CONFIG = {
-    introTitle : 'Em à!' ,
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    introTitle : 'Tường Vy à!' ,
+    introDesc: `Em có tin vào tình yêu từ cái nhìn đầu tiên không,
+    hay để anh thử đi lại qua em lần nữa?
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    title: ' Tình yêu thương chẳng hề hư mất bao giờ 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
@@ -13,7 +12,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: '..anh cũng yêu em💚💛'
+    messDesc: '.hẹn gặp em vào một ngyaf gần nhât💚 '
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/HoangLamNHL2001' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
